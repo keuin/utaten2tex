756d8549ae59f3bdd29b18111737c7d775bae304
Keuin <keuinx@gmail.com>
2023-06-04 00:43:35 +0800

    Set default behavior to not suggest file name, making PDF preview-able on browsers

--- web.py
+++ web.py
@@ -17,13 +17,18 @@ utaten_pattern = re.compile(r'[a-z0-9]+')
 tex_generator = texgen.TexGenerator('pdf_cache', 'temp', 20)
 html_cache = htmlcache.HtmlCache('html_cache')
 
+suggest_file_name = False
+
 
 @app.get("/utaten/{item_id}.pdf")
 async def get_utaten_lyric_pdf(item_id: str):
+    global suggest_file_name
     try:
         lyric_info = await html_cache.get_utaten_tex_source(item_id)
         pdf_path = await tex_generator.xelatex(lyric_info.tex_source)
-        if lyric_info.title and lyric_info.artist:
+        if not suggest_file_name:
+            filename = None
+        elif lyric_info.title and lyric_info.artist:
             filename = f'{lyric_info.title} - {lyric_info.artist}.pdf'
         elif not lyric_info.title and not lyric_info.artist:
             filename = f'{lyric_info.utaten_id}.pdf'
@@ -63,7 +68,9 @@ if __name__ == '__main__':
     p = argparse.ArgumentParser(prog='utaten2tex')
     p.add_argument('-l', '--host', default='127.0.0.1')
     p.add_argument('-p', '--port', default='8080')
+    p.add_argument('-s', '--suggest-file-name', action='store_true', default=False)
     args = p.parse_args()
+    suggest_file_name = args.suggest_file_name
     setup_loop()
     uvicorn.run(
         'web:app',
